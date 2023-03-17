86d3578b419502dec2d6d5611814c6939d33c385
Pedro Soares <dev.pedro.nogueira@gmail.com>
2023-03-17 18:17:10 -0300

    feat:: dashboard amount  details

--- src/components/admin/Summary.tsx
+++ src/components/admin/Summary.tsx
@@ -4,16 +4,44 @@ import { z } from "zod"
 import DatePicker from "react-datepicker"
 import "react-datepicker/dist/react-datepicker.css"
 import ptBR from "date-fns/locale/pt-BR"
+import { useAppSelector } from "@/hooks/useReducer"
+import { format } from "date-fns"
+import { priceFormatter } from "@/utils/formatter"
+import { parseISO, isAfter } from "date-fns"
+import { IOrdersDetails } from "@/interfaces"
 
 const Summary = () => {
+  const { orders } = useAppSelector((state) => state.orders)
   const [initialDate, setInitialDate] = useState<Date | null>()
   const [finalDate, setFinalDate] = useState<Date | null>()
+  const [filteredOrders, setFilteredOrders] = useState(orders)
+
+  const summaryOrders =
+    filteredOrders.length > 0 ? filteredOrders.length : orders.length
+  const summaryAmount =
+    filteredOrders.length > 0
+      ? filteredOrders.reduce((amount, order) => amount + order.price_amount, 0)
+      : orders.reduce((amount, order) => amount + order.price_amount, 0)
 
   const filter = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault()
 
-    console.log(initialDate)
-    console.log(finalDate)
+    if (initialDate && finalDate) {
+      const initFormatterd = format(initialDate!, "dd/MM/yyyy")
+      const fimFormatterd = format(finalDate!, "dd/MM/yyyy")
+
+      const filterdOrders = orders.filter((order) => {
+        if (initFormatterd <= format(new Date(order.createdAt), "dd/MM/yyyy")) {
+          if (
+            fimFormatterd >= format(new Date(order.createdAt), "dd/MM/yyyy")
+          ) {
+            return order
+          }
+        }
+      })
+
+      setFilteredOrders(filterdOrders)
+    }
   }
 
   return (
@@ -22,7 +50,7 @@ const Summary = () => {
       <form className="flex items-center justify-start text-center gap-5">
         <DatePicker
           selected={initialDate}
-          dateFormat="Pp"
+          dateFormat="dd/MM/yyyy"
           locale={ptBR}
           onChange={(date) => setInitialDate(date)}
           className="text-black w-full border border-gray-300 rounded-md px-4 py-2"
@@ -30,7 +58,7 @@ const Summary = () => {
         <p>até</p>
         <DatePicker
           selected={finalDate}
-          dateFormat="Pp"
+          dateFormat="dd/MM/yyyy"
           locale={ptBR}
           onChange={(date) => setFinalDate(date)}
           className="bg-white w-full border border-gray-300 rounded-md px-4 py-2"
@@ -44,18 +72,16 @@ const Summary = () => {
           Filtrar
         </button>
       </form>
-      <div className="grid grid-cols-1 md:grid-cols-3 w-full gap-5">
+      <div className="grid grid-cols-1 md:grid-cols-2 w-full gap-5">
         <span className="bg-[#006E71] bg-opacity-20 rounded-md p-4 w-full space-y-2 text-gray-800">
-          <p className="text-md">Pedidos por dia</p>
-          <p className="text-3xl font-bold">22</p>
+          <p className="text-md">Pedidos do periodo</p>
+          <p className="text-3xl font-bold">{summaryOrders}</p>
         </span>
         <span className="bg-[#006E71] bg-opacity-20 rounded-md p-4 w-full space-y-2 text-gray-800">
-          <p className="text-md">Pedidos por dia</p>
-          <p className="text-3xl font-bold">R$355,80</p>
-        </span>
-        <span className="bg-[#006E71] bg-opacity-20 rounded-md p-4 w-full space-y-2 text-gray-800">
-          <p className="text-md">Pedidos por periodo</p>
-          <p className="text-3xl font-bold">R$930,25</p>
+          <p className="text-md">Total do periodo</p>
+          <p className="text-3xl font-bold">
+            {priceFormatter.format(summaryAmount / 100)}
+          </p>
         </span>
       </div>
     </div>
