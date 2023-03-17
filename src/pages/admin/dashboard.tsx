29a110d16b46375f41964f5aeb23ebe50461f21d
Pedro Soares <dev.pedro.nogueira@gmail.com>
2023-03-17 18:53:37 -0300

    refactor:: filters on dashboard

--- src/pages/admin/dashboard.tsx
+++ src/pages/admin/dashboard.tsx
@@ -11,6 +11,8 @@ import Tag from "@/components/designSystem/Tag"
 import { useAppDispatch, useAppSelector } from "@/hooks/useReducer"
 import { ordersActions } from "@/reducers/features/ordersSlice"
 import Summary from "@/components/admin/Summary"
+import DatePicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css"
 
 const Dashboard = () => {
   const { orders, loading, statusOnOrder } = useAppSelector(
@@ -32,6 +34,39 @@ const Dashboard = () => {
     setOrderByStatus(filteredOrdersByStatus)
   }
 
+  const [initialDate, setInitialDate] = useState<Date | null>()
+  const [finalDate, setFinalDate] = useState<Date | null>()
+  const [filteredOrders, setFilteredOrders] = useState(orders)
+
+  const summaryOrders =
+    filteredOrders.length > 0 ? filteredOrders.length : orders.length
+
+  const summaryAmount =
+    filteredOrders.length > 0
+      ? filteredOrders.reduce((amount, order) => amount + order.price_amount, 0)
+      : orders.reduce((amount, order) => amount + order.price_amount, 0)
+
+  const filter = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault()
+
+    if (initialDate && finalDate) {
+      const initFormatterd = format(initialDate!, "dd/MM/yyyy")
+      const fimFormatterd = format(finalDate!, "dd/MM/yyyy")
+
+      const filterdOrders = orders.filter((order) => {
+        if (initFormatterd <= format(new Date(order.createdAt), "dd/MM/yyyy")) {
+          if (
+            fimFormatterd >= format(new Date(order.createdAt), "dd/MM/yyyy")
+          ) {
+            return order
+          }
+        }
+      })
+
+      setFilteredOrders(filterdOrders)
+    }
+  }
+
   return (
     <>
       <Head>
@@ -44,22 +79,61 @@ const Dashboard = () => {
             Bem vindo, Boozer admin
           </h1>
 
-          <Summary />
+          <div className="w-full space-y-5 border border-gray-300 p-4 rounded-md">
+            <p>Selecione a data e veja detalhes dos pedidos por periodo</p>
+            <form className="flex items-center justify-start text-center gap-5">
+              <DatePicker
+                selected={initialDate}
+                dateFormat="dd/MM/yyyy"
+                locale={ptBR}
+                onChange={(date) => setInitialDate(date)}
+                className="text-black w-full border border-gray-300 rounded-md px-4 py-2"
+              />
+              <p>até</p>
+              <DatePicker
+                selected={finalDate}
+                dateFormat="dd/MM/yyyy"
+                locale={ptBR}
+                onChange={(date) => setFinalDate(date)}
+                className="bg-white w-full border border-gray-300 rounded-md px-4 py-2"
+              />
 
-          <div>
-            <p>Selecione o status para filtrar</p>
+              <button
+                onClick={(e) => filter(e)}
+                className="py-[10px] px-4 rounded-lg font-bold bg-green-700 text-white hover:bg-green-600 
+                transition-all disabled:opacity-25 w-full max-w-[12rem]"
+              >
+                Filtrar
+              </button>
+            </form>
+            <div className="grid grid-cols-1 md:grid-cols-2 w-full gap-5">
+              <span className="bg-[#006E71] bg-opacity-20 rounded-md p-4 w-full space-y-2 text-gray-800">
+                <p className="text-md">Pedidos do periodo</p>
+                <p className="text-3xl font-bold">{summaryOrders}</p>
+              </span>
+              <span className="bg-[#006E71] bg-opacity-20 rounded-md p-4 w-full space-y-2 text-gray-800">
+                <p className="text-md">Total do periodo</p>
+                <p className="text-3xl font-bold">
+                  {priceFormatter.format(summaryAmount / 100)}
+                </p>
+              </span>
+            </div>
 
-            <div className="flex items-center justify-start gap-4">
-              {statusOnOrder.map((status) => {
-                return (
-                  <button
-                    key={status.id}
-                    onClick={(e) => ordersByStatus(status.id)}
-                  >
-                    <Tag title={status.title} />
-                  </button>
-                )
-              })}
+            <div className="space-y-5">
+              <p>Selecione o status para filtrar</p>
+
+              <div className="flex items-center justify-start gap-4">
+                {statusOnOrder.map((status) => {
+                  return (
+                    <button
+                      key={status.id}
+                      onClick={(e) => ordersByStatus(status.id)}
+                    >
+                      <Tag title={status.title} />
+                    </button>
+                  )
+                })}
+              </div>
             </div>
           </div>
 
